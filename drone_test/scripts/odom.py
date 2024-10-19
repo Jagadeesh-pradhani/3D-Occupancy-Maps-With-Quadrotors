6d257ede7058ae3d151382afd3a5237c9dc78f7a
jagadeesh-pradhani <01fe21bee114@kletech.ac.in>
2024-10-19 16:42:15 +0000

    Live update

--- drone_test/scripts/odom.py
+++ drone_test/scripts/odom.py
@@ -7,14 +7,14 @@ from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 
 rospy.init_node('odom_pub')
 
-odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+odom_pub = rospy.Publisher('odom', Odometry, queue_size=10)
 
 rospy.wait_for_service('gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
 
 odom = Odometry()
 header = Header()
-header.frame_id = '/odom'
+header.frame_id = 'odom'
 
 model = GetModelStateRequest()
 model.model_name = 'quadrotor'
--- octomap_mapping/octomap_server/src/pub.cpp
+++ octomap_mapping/octomap_server/src/pub.cpp
@@ -11,6 +11,8 @@
 #include <octomap/OcTree.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap/octomap_types.h>
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
 
 using namespace std;
 using namespace octomap;
@@ -18,13 +20,17 @@ using namespace octomap;
 ros::Publisher _local_map_pub;
 ros::Publisher _all_map_pub;
 ros::Subscriber _odom_sub;
+ros::Subscriber _camera_pointcloud_sub;
 
 bool _map_ok = false;
 bool _has_odom = false;
 
 sensor_msgs::PointCloud2 localMap_pcd;
 sensor_msgs::PointCloud2 globalMap_pcd;
-pcl::PointCloud<pcl::PointXYZ> cloudMap;
+pcl::PointCloud<pcl::PointXYZ> cloudMap;   // The global map
+pcl::PointCloud<pcl::PointXYZ> cameraCloud; // The live point cloud from the camera
+
+tf::TransformListener* tf_listener;
 
 void loadSavedMap(const std::string& map_file) {
     // Load the saved OctoMap from the file
@@ -60,11 +66,41 @@ void loadSavedMap(const std::string& map_file) {
     delete octree;
 }
 
+void cameraPointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& input) {
+    // Transform the camera point cloud to the world frame
+    sensor_msgs::PointCloud2 transformed_cloud;
+    try {
+        tf_listener->waitForTransform("world", input->header.frame_id, ros::Time(0), ros::Duration(3.0));
+        pcl_ros::transformPointCloud("world", *input, transformed_cloud, *tf_listener);
+        ROS_INFO("Transformed point cloud from camera_depth_optical_frame to world frame.");
+    } catch (tf::TransformException &ex) {
+        ROS_ERROR("Transform error: %s", ex.what());
+        return;
+    }
+
+    // Convert the transformed PointCloud2 message to a pcl::PointCloud
+    pcl::fromROSMsg(transformed_cloud, cameraCloud);
+    
+    ROS_INFO("Received and transformed live point cloud from camera with %lu points", cameraCloud.points.size());
+
+    // Compare and update the global map with the camera point cloud data
+    for (const auto& point : cameraCloud.points) {
+        // Add points from the camera view to the global map if they aren't already in it
+        cloudMap.points.push_back(point);
+    }
+
+    cloudMap.width = cloudMap.points.size();
+    cloudMap.height = 1;
+    cloudMap.is_dense = true;
+
+    ROS_WARN("Updated global map with live camera data in world frame.");
+}
+
 void pubSensedPoints() {
     if (!_map_ok)
         return;
 
-    // Publish the loaded map as a ROS PointCloud2 message
+    // Publish the updated map as a ROS PointCloud2 message
     pcl::toROSMsg(cloudMap, globalMap_pcd);
     globalMap_pcd.header.frame_id = "world";
     _all_map_pub.publish(globalMap_pcd);
@@ -80,6 +116,12 @@ int main(int argc, char** argv) {
     std::string map_file;
     n.param("map_file", map_file, std::string("/home/ros/btraj/my_octomap.ot"));
 
+    // Initialize the tf listener
+    tf_listener = new tf::TransformListener();
+
+    // Subscribe to the camera/depth/points topic
+    _camera_pointcloud_sub = n.subscribe("/camera/depth/points", 1, cameraPointCloudCallback);
+
     // Load the saved map
     loadSavedMap(map_file);
 
@@ -91,5 +133,6 @@ int main(int argc, char** argv) {
         loop_rate.sleep();
     }
 
+    delete tf_listener;
     return 0;
 }
